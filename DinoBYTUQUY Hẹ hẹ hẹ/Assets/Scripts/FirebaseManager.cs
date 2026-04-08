23c0b4f7d828aad11f0ef6c2542d4f3bda8cc663
Tran Nhat Tam <caubetam2004@gmail.com>
2026-04-08 18:14:54 +0700

    feat: add Link Firebase

--- DinoBYTUQUY Hẹ hẹ hẹ/Assets/Scripts/FirebaseManager.cs
+++ DinoBYTUQUY Hẹ hẹ hẹ/Assets/Scripts/FirebaseManager.cs
@@ -21,3 +21,36 @@ public class FirebaseManager : MonoBehaviour
         public string name;
         public int score;
     }
+    
+    // 1. GỌI KHI GAME OVER
+    public void GameOver(int score)
+    {
+        string playerName = "Khách Không Tên";
+        if (nameInput != null && !string.IsNullOrEmpty(nameInput.text))
+        {
+            playerName = nameInput.text;
+        }
+
+        StartCoroutine(SendScore(playerName, score));
+    }
+
+    IEnumerator SendScore(string name, int score)
+    {
+        string json = "{\"name\":\"" + name + "\",\"score\":" + score + "}";
+
+        using (UnityWebRequest www = UnityWebRequest.PostWwwForm(firebaseUrl, "POST"))
+        {
+            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(json);
+            www.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            www.downloadHandler = new DownloadHandlerBuffer();
+            www.SetRequestHeader("Content-Type", "application/json");
+
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("✅ Đã bắn điểm lên mạng! Đang lấy bảng xếp hạng về...");
+                StartCoroutine(GetLeaderboard());
+            }
+        }
+    }
